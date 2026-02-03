15edd3f38b7fd50e9cc5b95b54b93fb83645319f
rildian <rildian.nbeserra@gmail.com>
2026-02-02 21:51:30 -0300

    fix: changed the args used in the cwebp

--- convert_all_webp/src/conversor.sh
+++ convert_all_webp/src/conversor.sh
@@ -10,7 +10,7 @@ image_conversor() {
 
     for dir in "${IMAGE_DIR[@]}"; do
         for file in "$dir"/*; do
-            cwebp -lossless "$file" -o "${file%.*}.webp" &>/dev/null && rm "$file"
+            cwebp -q 80 -m 6 "$file" -o "${file%.*}.webp" &>/dev/null && rm "$file"
         done
     done
 }
