4211be95e71d7332a5bb0b20ad4bbb88a5844eca
rildian <rildian.nbeserra@gmail.com>
2026-02-16 21:54:18 -0300

    chore: verification comes 1st

--- automate_git_commit/src/git_commit.sh
+++ automate_git_commit/src/git_commit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd "$1" || exit 1
-
 if [ -z "$1" ]; then
     echo "Error: No repository path provided."
     exit 1
 fi
 
+cd "$1" || exit 1
+
 git_commit() {
-    git add .
+    git add . &>/dev/null
     git commit -m "up $(date)" &>/dev/null
     git push origin main &>/dev/null
 }
