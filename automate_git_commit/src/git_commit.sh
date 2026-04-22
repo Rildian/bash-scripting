64c3523e3b518f9e8f34c90b70bcd964b68a61bb
Rildian <rildian.nbeserra@gmail.com>
2026-04-22 16:32:26 -0300

    fix: just pulling b4 commiting

--- automate_git_commit/src/git_commit.sh
+++ automate_git_commit/src/git_commit.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Error: No repository path provided."
-    exit 1
+  echo "Error: No repository path provided."
+  exit 1
 fi
 
 cd "$1" || exit 1
 
 git_commit() {
-    git add . &>/dev/null
-    git commit -m "up $(date)" &>/dev/null
-    git push origin main &>/dev/null
+  git pull origin main &>/dev/null
+  git add . &>/dev/null
+  git commit -m "up $(date)" &>/dev/null
+  git push origin main &>/dev/null
 }
 
 git_commit
